8541fa1258b953a5706f8dc4cb7ac66c3d283ac3
Joel Lange <jonbtm@gmail.com>
2025-01-24 12:20:57 -0500

    added allow ping to .cmd

--- scripts/SetupComplete.cmd
+++ scripts/SetupComplete.cmd
@@ -1,6 +1,5 @@
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTPS-In)" new action=allow
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new action=allow
-netsh advfirewall firewall set rule name="File and Printer Sharing (Echo Request - ICMPv4-In)" new action=allow
-netsh advfirewall firewall set rule name="File and Printer Sharing (Echo Request - ICMPv6-In)" new action=allow
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 powershell -File C:\Windows\Setup\Scripts\WinRMCert.ps1
 del C:\Windows\Panther\autounattend.xml
