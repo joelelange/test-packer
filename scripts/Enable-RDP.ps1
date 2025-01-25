b6f3f666a017716bfdc59a5538ae297956bbf267
Joel Lange <jonbtm@gmail.com>
2025-01-24 19:34:20 -0500

    corrected ps scripts

--- scripts/Enable-RDP.ps1
+++
@@ -1,5 +0,0 @@
-# Enable RDP
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
-
-# Create firewall rule to allow RDP
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
--- scripts/SetupComplete.cmd
+++ scripts/SetupComplete.cmd
@@ -1,5 +1,4 @@
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTPS-In)" new action=allow
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new action=allow
-netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 powershell -File C:\Windows\Setup\Scripts\WinRMCert.ps1
 del C:\Windows\Panther\autounattend.xml
